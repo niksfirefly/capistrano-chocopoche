7db0b772cdfb211451a253f82064cf1ae1d6659b
nikSFirefly <niksfirefly@gmail.com>
2013-12-24 11:47:11 +0100

    Update chocopoche.rb

--- lib/capistrano/chocopoche.rb
+++ lib/capistrano/chocopoche.rb
@@ -1,5 +1,5 @@
 require "capistrano/chocopoche/version"
-require 'capistrano/chocopoche/railsless-deploy'
+# require 'capistrano/chocopoche/railsless-deploy'
 require 'capistrano/chocopoche/files'
 require 'capistrano/chocopoche/mysql'
 require 'capistrano/chocopoche/composer'
